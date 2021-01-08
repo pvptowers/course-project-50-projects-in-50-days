192242eca7dd7535dd24b06a5e43d0ce26a1838b
Paul Towers <paultowers2005@yahoo.com.au>
2021-01-08 19:56:37 +1100

    adding start of JS for drink water project

---
+++ drink-water/script.js
@@ -0,0 +1,38 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const litres = document.getElementById('litres')
+const percentage = document.getElementById('pecentage')
+const remained = document.getElementById('remained')
+
+updateBigCup()
+
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener('click', () => highlightCups(idx))
+})
+
+function highlightCups(idx) {
+    if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+    smallCups.forEach((cup, idx2) => {
+        if (idx2 <= idx) {
+            cup.classList.add('full')
+        } else {
+            cup.classList.remove('full')
+        }
+    })
+
+    updateBigCup()
+}
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll('.cup-small.full').length
+    const totalCups = smallCups.length
+
+    if (fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+    }
+}
