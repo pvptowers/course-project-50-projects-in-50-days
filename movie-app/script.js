3fba97d6676fce33a135917b9349382d6f87e4b5
Paul Towers <paultowers2005@yahoo.com.au>
2021-01-17 20:26:05 +1100

    css and JS bug fixes

--- movie-app/script.js
+++ movie-app/script.js
@@ -1,20 +1,21 @@
-const API_URL = ""
+//PLEASE ADD YOUR OWN API KEY - YOUR CAN REGISTER FOR ONE AT THEMOBIVEDB.ORG
+let apikey = ''
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apikey}&page=1`
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
-//Get initial movies
+
+// Get initial movies
 getMovies(API_URL)
 
 async function getMovies(url) {
     const res = await fetch(url)
-    const data = await res.json
+    const data = await res.json()
+
     showMovies(data.results)
-    console.log(data.results)
-
-
 }
 
 function showMovies(movies) {
@@ -27,22 +28,22 @@ function showMovies(movies) {
             vote_average,
             overview
         } = movie
+
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
+
         movieEl.innerHTML = `
-
-            <img src="${IMG_PATH + poster_path}" alt="${title}"></img>
-            <div class="movie-info"></div>
-            <h3>${title}</h3>
-            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
         </div>
-        <div class="overview">
-            <h3>Overview</h3>
-            ${overview}
-        </div>`
-
+        `
         main.appendChild(movieEl)
-
     })
 }
 
@@ -58,13 +59,14 @@ function getClassByRate(vote) {
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    const searchTerm = search.nodeValue
 
-    if (searchTerm && searchTerm !== "") {
+    const searchTerm = search.value
+
+    if (searchTerm && searchTerm !== '') {
         getMovies(SEARCH_API + searchTerm)
 
-        search.value = ""
+        search.value = ''
     } else {
-        windows.location.reload()
+        window.location.reload()
     }
 })
